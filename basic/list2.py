52d688b7c37312496baaec85693a71bd536beacf
Rafael Beirigo <rafaelbeirigo@gmail.com>
2012-08-18 23:40:55 +0000

    Terminei o item *E*

--- basic/list2.py
+++ basic/list2.py
@@ -38,16 +38,25 @@ def remove_adjacent(nums):
 # Ideally, the solution should work in "linear" time, making a single
 # pass of both lists.
 def linear_merge(list1, list2):
-  # +++your code here+++
-  return
+  i = 0
+  j = 0
+  result = []
+  while i < len(list1) and j < len(list2):
+    if list1[i] < list2[j]:
+      result.append(list1[i])
+      i += 1
+    else:
+      result.append(list2[j])
+      j += 1
 
-# Note: the solution above is kind of cute, but unforunately list.pop(0)
-# is not constant time with the standard python list implementation, so
-# the above is not strictly linear time.
-# An alternate approach uses pop(-1) to remove the endmost elements
-# from each list, building a solution list which is backwards.
-# Then use reversed() to put the result back in the correct order. That
-# solution works in linear time, but is more ugly.
+  # verifica se uma das listas ainda contem itens e,
+  # em caso afirmativo, adiciona-os a lista resultante
+  if i < len(list1):
+    result.extend(list1[i:])
+  elif j < len(list2):
+    result.extend(list2[j:])
+  
+  return result
 
 
 # Simple provided test() function used in main() to print
